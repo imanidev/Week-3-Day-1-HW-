c2a63e01eb03215d69b2b28e9b1db81df6048d30
imani.devs <imaniroberts.dev@gmail.com>
2023-03-12 19:18:36 -0400

    used two different methods for problem 3

--- Week-3-Day-1-HW.js
+++ Week-3-Day-1-HW.js
@@ -1,4 +1,4 @@
-// 1.
+// Problem 1
 
 let maxOfTwoNumbers = function (num1, num2) {
     if (num1 >= num2) {
@@ -12,7 +12,7 @@ let maxOfTwoNumbers = function (num1, num2) {
 
 console.log(maxOfTwoNumbers(3, 10))
 
-//2.
+//Problem 2
 
 let maxOfThree = function (num1, num2, num3) {
     if (num1 > num2 && num3) {
@@ -29,3 +29,31 @@ let maxOfThree = function (num1, num2, num3) {
 // let maxOfThree = (num1, num2, num3) => num1 > num2 && num1 > num3 ? num1 : num2 > num1 && num2 > num3 ? num2 : num3;
 console.log(maxOfThree(2, 56, 12))
 maxOfThree()
+
+
+//Problem 3 
+
+//method 1
+function isCharAVowel(char) {
+    char = char.toLowerCase()
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === 'y') {
+        return true
+    } else {
+        return false
+    }
+}
+
+//method 2
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+
+function isCharAVowel(char) {
+    char = char.toLowerCase()
+    for (let i = 0; i < vowels.length; i++) {
+        if (vowels.includes(char)) {
+            return `Hooray! ${char} is a vowel!`
+        } else {
+            return `Bummer dude, ${char} is not a vowel.`
+        }
+    }
+}
+console.log(isCharAVowel('a'))
