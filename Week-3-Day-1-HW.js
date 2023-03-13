98b130f0bb69f416c242171bea2d475bdf7f76ef
imani.devs <imaniroberts.dev@gmail.com>
2023-03-13 13:16:00 -0400

    added work

--- Week-3-Day-1-HW.js
+++ Week-3-Day-1-HW.js
@@ -57,3 +57,76 @@ function isCharAVowel(char) {
     }
 }
 console.log(isCharAVowel('a'))
+
+//Problem 4
+
+const arrayOfNums = [4, 6, 9, 2, 18, 5, 10]
+
+let sumArray = function (array) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum
+}
+console.log(sumArray(arrayOfNums))
+
+
+//Problem 5 
+
+const itsAnArray = [1, 3, 57, 9, 11]
+
+function multiplyArray(array) {
+    let multiplication = 1;
+    for (let i = 0; i < array.length; i++) {
+        multiplication *= array[i]
+    }
+    return multiplication
+}
+
+console.log(multiplyArray(itsAnArray))
+
+//Problem 6
+//Define a function, as a function expression, numArgsthat returns the number of arguments passed to the function when called.
+
+let numArgs = function () {
+    return arguments.length
+}
+console.log(numArgs(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+
+//Problem 7
+
+function reverseString(string) {
+    let splitString = string.split('')
+    let reverseString = splitString.reverse()
+    let joinString = reverseString.join('')
+    return joinString
+}
+console.log(reverseString('rockstar'))
+
+//Problem 8
+
+let longestStringInArray = function (array) {
+    let longestString = ''
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > longestString.length) {
+            longestString = array[i]
+        }
+    }
+    return longestString.length
+}
+console.log(longestStringInArray(['pizza', 'green', 'thisIsNotATest', 'HelloWorld']))
+
+//Problem 9
+
+
+function stringsLongerThan(array, num) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > num) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+console.log(stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3))
